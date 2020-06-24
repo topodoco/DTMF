2dc6318cd660fd3208da506adfe58a6bd4623bc7
Octave GENEST <octave.genest@epita.fr>
2020-06-24 23:39:52 +0200

    [Decoder][1rst commit] I have some compilations errors that I'm not able to solve (library problems)

---
+++ decoder.py
@@ -0,0 +1,38 @@
+from argparse import ArgumentParser
+from pathlib import Path
+
+import scipy.io as sio
+import numpy as np
+import matplotlib.pyplot as plt
+#import time
+
+def Print_Graph(filename):
+    print(filename)
+    samplerate, data = sio.wavfile.read(filename)
+
+    print(f"number of channels = {data.shape[1]}")
+
+    length = data.shape[0] / samplerate
+    print(f"length = {Length}s")
+
+    time = np.linspace(0., length, data.shape[0])
+    plt.plot(time, data[:, 0], label = "Left channel")
+    plt.plot(time, data[:, 1], label = "Rigth channel")
+    plt.legend()
+    plt.xlabel("time[s]")
+    plt.ylabel("Amplitude")
+    plt.show()
+
+    return 0
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description = "DTMF decoder")
+    parser.add_argument('Audio_File', help = "need a .wav file")
+
+    args = parser.parse_args()
+
+    print(args.Audio_File)
+    file = Path(args.Audio_File).absolute()
+    print(file)
+
+    Print_Graph(file)
